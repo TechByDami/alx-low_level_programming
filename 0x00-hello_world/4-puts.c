0992f4bcd1d2336a961b2506b8f14e429ed579ef
TechByDami <agboolaolawale001@gmail.com>
2022-09-08 20:02:35 -0700

    C that print function puts :earth_africa::trophy::nigeria::fire:

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program that prints a function followed by a new line
+ * Return; 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
