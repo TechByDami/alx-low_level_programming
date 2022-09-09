7505aa12f00048e1871579d55fd0d6c7cef52d8e
TechByDami <agboolaolawale001@gmail.com>
2022-09-08 20:30:35 -0700

    C that prints function printf :earth_africa::trophy::nigeria::fire:

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints using printf
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
