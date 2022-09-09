117d66e16558f5f8b3806061582f4fc51b73a65d
TechByDami <agboolaolawale001@gmail.com>
2022-09-08 20:49:22 -0700

    Print sizeof :earth_africa::trophy::nigeria::fire:

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+int main(void)
+{
+
+	/* sizeof evaluates the size of a variable */
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+
+}
