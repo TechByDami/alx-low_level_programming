519e9af1f17d75aac1010fc8f609a0adcaf435ef
TechByDami <agboolaolawale001@gmail.com>
2022-09-13 15:15:11 -0700

    Print _putchar :earth_africa::trophy::nigeria::fire:

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - function that prints _putchar
+ * Return: returns 0 if successful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
