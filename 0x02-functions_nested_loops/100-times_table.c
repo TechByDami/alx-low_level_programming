3a8e78a0891b7b50a7f59b1357725868b65c691a
TechByDami <agboolaolawale001@gmail.com>
2022-09-13 16:55:32 -0700

    Print the n times table, starting with 0 :earth_africa::trophy::nigeria::fire:

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - prints the n times table
+ * @n: the upper limit to be printed
+ */
+void print_times_table(int n)
+{
+	int row;
+	int column;
+	int product;
+
+	if (n < 0 || n >= 15)
+		return;
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			product = (row * column);
+			if (column == 0)
+				_putchar('0' + product);
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (product / 10));
+					_putchar('0' + (product % 10));
+				}
+				else if (product >= 100)
+				{
+					_putchar('0' + (product / 100));
+					_putchar('0' + ((product / 10) % 10));
+					_putchar('0' + (product % 10));
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
