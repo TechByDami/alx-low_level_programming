600a937a8156ca737f2d7eb777673a6286455473
TechByDami <agboolaolawale001@gmail.com>
2022-09-13 16:51:43 -0700

    Add two integers and return result :earth_africa::trophy::nigeria::fire:

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers togethsr
+ * @a: one argument
+ * @b: another argument to be added
+ * Return: returns 0 if successful
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
