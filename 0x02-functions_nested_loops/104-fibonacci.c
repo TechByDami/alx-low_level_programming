4ec3df4c0215bf187a87e580adeabd7ee2deee75
TechByDami <agboolaolawale001@gmail.com>
2022-09-14 04:43:08 -0700

    Fix to betty style :earth_africa::trophy::nigeria::fire:

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,9 +8,9 @@ int main(void)
 	int count;
 	unsigned long i, j, k;
 	unsigned long m, n, p, carry;
-	count = 0;
-	i = 0;
-	j = 1;
+		count = 0;
+		i = 0;
+		j = 1;
 	for (count = 1; count <= 91; count++)
 	{
 		k = i + j;
