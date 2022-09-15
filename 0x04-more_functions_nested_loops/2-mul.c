96d782d1890616a795ef3f3f48bee2c5211dc6b4
TechByDami <agboolaolawale001@gmail.com>
2022-09-15 03:26:44 -0700

    Multiplies two integers :earth_africa::trophy::nigeria::fire:

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multipies two integers
+ * @a: number1
+ * @b: number2
+ * Return: The multiplication result of the numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
