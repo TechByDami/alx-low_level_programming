375a6737c7f5d052c4486b8479d22350f6b27f24
TechByDami <agboolaolawale001@gmail.com>
2022-09-15 04:28:19 -0700

    Print 10 times the numbers, from 0 to 14 :earth_africa::trophy::nigeria::fire:

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14,
+ * followed by a new line.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
+}
