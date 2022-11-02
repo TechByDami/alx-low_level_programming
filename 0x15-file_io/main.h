eb182871a7b0bf51dfc4c8c526e55ecfc335ed32
TechByDami <agboolaolawale001@gmail.com>
2022-11-01 22:21:10 -0700

    added prptptypes pf functions :earth_africa::trophy::nigeria::fire:

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _main_h
+#define _main_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
