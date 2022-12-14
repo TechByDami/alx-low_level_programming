b8da84af4d7aadbd7337d9bfd32bd68e4336fd39
TechByDami <agboolaolawale001@gmail.com>
2022-12-14 04:03:00 -0800

    creates a dynamic library called liball.so :earth_africa::trophy::nigeria::fire:

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -wall -wextra -werror -pedantic -c -fPIC -c *.c
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC -c *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
